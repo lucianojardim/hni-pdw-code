a967879c185d7eb46a5cb167a1fac205b7569912
LucianoJ <lucianojardim@users.noreply.github.com>
2013-11-20 16:59:18 -0500

    case 364 - added more validation for my account

--- PDWModels/Users/MyAccountInfo.cs
+++ PDWModels/Users/MyAccountInfo.cs
@@ -14,31 +14,42 @@ namespace PDWModels.Users
 		[DisplayName( "Email Address" )]
 		[Required]
 		[Email( ErrorMessage = "Email Address doesn't look like a valid email address." )]
+		[StringLength(50)]
 		public string EmailAddress { get; set; }
 		[DisplayName( "First Name" )]
 		[Required]
+		[StringLength( 50 )]
 		public string FirstName { get; set; }
 		[DisplayName( "Last Name" )]
 		[Required]
+		[StringLength( 50 )]
 		public string LastName { get; set; }
 		[DisplayName( "Company Name" )]
 		[Required]
+		[StringLength( 50 )]
 		public string CompanyName { get; set; }
 		[DisplayName( "Address 1" )]
+		[StringLength( 100 )]
 		public string Address1 { get; set; }
 		[DisplayName( "Address 2" )]
+		[StringLength( 100 )]
 		public string Address2 { get; set; }
 		[DisplayName( "City" )]
+		[StringLength( 50 )]
 		public string City { get; set; }
 		[DisplayName( "State" )]
 		public string State { get; set; }
 		[DisplayName( "Zip Code" )]
+		[PostalCode]
 		public string Zip { get; set; }
 		[DisplayName( "Business Phone" )]
+		[PhoneNumber]
 		public string BusinessPhone { get; set; }
 		[DisplayName( "Cell Phone" )]
+		[PhoneNumber]
 		public string CellPhone { get; set; }
 		[DisplayName( "Title" )]
+		[StringLength( 50 )]
 		public string Title { get; set; }
 	}
 }
