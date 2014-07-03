20a20162243f5e685a414e0220ff32b66433c73d
LucianoJ <lucianojardim@users.noreply.github.com>
2014-07-03 08:47:56 -0400

    more james changes

--- PDWInfrastructure/PaoliWebUser.cs
+++ PDWInfrastructure/PaoliWebUser.cs
@@ -259,6 +259,7 @@ namespace PDWInfrastructure
 		public bool CanManageUsers { get { return OneOfRoles( PaoliWebRole.SuperAdmin, PaoliWebRole.PaoliMemberAdmin ); } }
 		public bool CanManageCompanies { get { return OneOfRoles( PaoliWebRole.SuperAdmin ); } }
 
+		public bool CanSeeYouniversity { get { return OneOfRoles( PaoliWebRole.PaoliSalesRep ); } }
 		public bool CanSeeTheScoop { get { return OneOfRoles( PaoliWebRole.PaoliSalesRep ); } }
 		public bool HasContacts { get { return OneOfRoles( PaoliWebRole.PaoliSalesRep ) || IsDealerUser; } }
 
