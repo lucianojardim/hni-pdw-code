362dfa154cddfa33a83f3f622c7762c4c3c2281b
LucianoJ <lucianojardim@users.noreply.github.com>
2015-07-02 10:31:20 -0400

    staging server update

--- ProductDataWarehouse/Controllers/BaseController.cs
+++ ProductDataWarehouse/Controllers/BaseController.cs
@@ -18,10 +18,14 @@ namespace ProductDataWarehouse.Controllers
 				return "http://localhost:8787";
 			else if( url.Contains( "matt3400" ) )
 				return "http://matt3400.wdd.local:8787";
-			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
-				return "http://desktop.paoli.getvitaminj.com";
 			else if( url.Contains( "paoli-test01" ) )
 				return "http://paoli-test01";
+			else if( url.Contains( "paoli-test" ) )
+				return "http://paoli-test.getvitaminj.com";
+			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
+				return "http://desktop.paoli.getvitaminj.com";
+			else if( url.Contains( "192" ) )
+				return string.Format( "http://{0}", url );
 
 			return "http://desktop.paoli.com";
 		}
@@ -34,10 +38,14 @@ namespace ProductDataWarehouse.Controllers
 				return "http://localhost:8787";
 			else if( url.Contains( "matt3400" ) )
 				return "http://matt3400.wdd.local:8787";
-			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
-				return "http://paoli-test01";
 			else if( url.Contains( "paoli-test01" ) )
 				return "http://paoli-test01";
+			else if( url.Contains( "paoli-test" ) )
+				return "http://paoli-test01";
+			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
+				return "http://paoli.getvitaminj.com";
+			else if( url.Contains( "192" ) )
+				return string.Format( "http://{0}", url );
 
 			return "http://desktop.paoli.com";
 		}
@@ -50,10 +58,14 @@ namespace ProductDataWarehouse.Controllers
 				return "http://localhost:13801";
 			else if( url.Contains( "matt3400" ) )
 				return "http://matt3400.wdd.local:8780";
-			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
-				return "http://paoli-test01:8780";
 			else if( url.Contains( "paoli-test01" ) )
 				return "http://paoli-test01:8780";
+			else if( url.Contains( "paoli-test" ) )
+				return "http://paoli-test01:8780";
+			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
+				return "http://my.paoli.getvitaminj.com";
+			else if( url.Contains( "192" ) )
+				return string.Format( "http://{0}:8780", url );
 
 			return "http://my.paoli.com";
 		}
@@ -66,10 +78,14 @@ namespace ProductDataWarehouse.Controllers
 				return "http://localhost:13801";
 			else if( url.Contains( "matt3400" ) )
 				return "http://matt3400.wdd.local:8780";
-			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
-				return "http://my.paoli.getvitaminj.com";
 			else if( url.Contains( "paoli-test01" ) )
 				return "http://paoli-test01:8780";
+			else if( url.Contains( "paoli-test" ) )
+				return "http://my.paoli-test.getvitaminj.com";
+			else if( url.Contains( "jamesburnes" ) || url.Contains( "getvitaminj" ) )
+				return "http://my.paoli.getvitaminj.com";
+			else if( url.Contains( "192" ) )
+				return string.Format( "http://{0}:8780", url );
 
 			return "http://my.paoli.com";
 		}
